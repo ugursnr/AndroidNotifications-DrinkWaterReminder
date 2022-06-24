6a0c339f192debc946fe10bc9dd091406fc77a0a
ugursnr <ugurr.snr@gmal.com>
2022-06-24 19:18:41 +0300

    Notification feature will be implement.

---
+++ app/src/main/java/com/ugurrsnr/drinkwaterreminder/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.ugurrsnr.drinkwaterreminder
+
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.ugurrsnr.drinkwaterreminder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var sharedPreferences :SharedPreferences
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        sharedPreferences = getSharedPreferences("com.ugurrsnr.drinkwaterreminder", MODE_PRIVATE)
+        var currentDrink : Int = sharedPreferences.getInt("currentDrink", 0)
+        binding.drinkTodayTV.text = "You Drink : ${currentDrink.toString()}"
+        colorChanger(currentDrink)
+
+
+        binding.apply {
+
+            upButton.setOnClickListener {
+                currentDrink++
+                drinkTodayTV.text = "You Drink : ${currentDrink.toString()}"
+                colorChanger(currentDrink)
+                sharedPreferences.edit().putInt("currentDrink", currentDrink).apply()
+            }
+
+            downButton.setOnClickListener {
+                currentDrink--
+                if (currentDrink <0){
+                    currentDrink = 0
+                }
+                colorChanger(currentDrink)
+                drinkTodayTV.text = "You Drink : ${currentDrink.toString()}"
+                sharedPreferences.edit().putInt("currentDrink", currentDrink).apply()
+
+            }
+
+        }
+
+
+    }
+
+    private fun colorChanger(currentDrink : Int){
+
+        val redColor = ContextCompat.getColor(applicationContext,R.color.red)
+        val greenColor = ContextCompat.getColor(applicationContext,R.color.green)
+        val orangeColor = ContextCompat.getColor(applicationContext,R.color.orange)
+
+        when(currentDrink){
+            in 0..5 -> binding.currentTargetTV.setTextColor(redColor.toString().toInt())
+            in 5..8 -> binding.currentTargetTV.setTextColor(orangeColor.toString().toInt())
+            else    -> binding.currentTargetTV.setTextColor(greenColor.toString().toInt())
+
+        }
+
+    }
+
+
+
+
+
+
+}
